84dffbfa2b62d73b9e8a36fdd0d577e19ceb194a
aimeesoto <asoto42@uic.edu>
2026-09-22 23:31:04 -0500

    Gold medals by country

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -4,3 +4,21 @@ git_default_branch()
 
 system("git checkout -b DEV")
 system("git push -u origin DEV")
+
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+olympics <- read_csv("olympics.csv")
+
+#Calculate new variable
+olympics <- olympics %>%
+  mutate(total.medals = gold + silver + bronze)
+
+#How many gold medals has each country won?
+gold_by_country <- olympics %>%
+  group_by(country) %>%
+  summarise(total_gold = sum(gold, na.rm = TRUE)) %>%
+  arrange(desc(total_gold))
+
+gold_by_country
